5b8f42b1152f363603e1b485c96895be44ffadb0
Alan MARCHAND <namnetes@outlook.fr>
2023-08-29 21:09:34 +0200

    Réécriture de start.ps1

--- start.ps1
+++ start.ps1
@@ -1,8 +1,24 @@
-# Démarrer l'application Flask (assurez-vous que la commande est correcte)
-Start-Process -NoNewWindow -FilePath "python" -ArgumentList "app.py"
+# Activez l'environnement virtuel
+. ./.venv/bin/activate
+
+# Lancez l'application Flask
+python ./app.py &
+
+# Récupérer les PID des instances Python attachées à app.py
+$pids = Get-Process | Where-Object {$_.Path -eq "python ./app.py"} | Select-Object -ExpandProperty Id
+
+# Afficher les PID
+Write-Host "Voici le PID de l'instance Python attachée à app.py : $pids"
 
 # Attendre un court instant pour que Flask démarre complètement
-Start-Sleep -Seconds 3
+Start-Sleep -s 3
 
-# Démarrer BrowserSync avec le fichier de configuration
-Start-Process -NoNewWindow -FilePath "cmd.exe" -ArgumentList @("/C", "'browser-sync.cmd start --config bs-config.js'") 
+# Lancez BrowserSync avec son fichier de configuration
+Start-Process -FilePath "browser-sync" -ArgumentList "--config ./bs-config.js"
+
+# Démarrez le navigateur par défaut pour ouvrir un nouvel onglet sur l'url localhost:3000
+if ([System.Environment]::GetEnvironmentVariable("BROWSER", "Machine")) {
+    Start-Process -FilePath "$env:BROWSER" -ArgumentList "http://localhost:3000"
+} else {
+    Start-Process -FilePath "explorer.exe" -ArgumentList "http://localhost:3000"
+}
