d57ce5e722f13582e4aeee698faf72d9cca19705
namnetes <64556721+namnetes@users.noreply.github.com>
2023-08-30 17:16:26 +0200

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,8 +1,8 @@
-# Activez l'environnement virtuel
-. ./.venv/bin/activate
+# Activez l'environnement virtuel 
+.\.venv\Scripts\activate.bat
 
 # Lancez l'application Flask
-python ./app.py &
+Start-Process -NoNewWindow python -ArgumentList "./app.py" 
 
 # Récupérer les PID des instances Python attachées à app.py
 $pids = Get-Process | Where-Object {$_.Path -eq "python ./app.py"} | Select-Object -ExpandProperty Id
@@ -14,11 +14,7 @@ Write-Host "Voici le PID de l'instance Python attachée à app.py : $pids"
 Start-Sleep -s 3
 
 # Lancez BrowserSync avec son fichier de configuration
-Start-Process -FilePath "browser-sync" -ArgumentList "--config ./bs-config.js"
+Start-Process -NoNewWindow -FilePath "cmd.exe" -ArgumentList "/c browser-sync start --config .\bs-config.js"
 
 # Démarrez le navigateur par défaut pour ouvrir un nouvel onglet sur l'url localhost:3000
-if ([System.Environment]::GetEnvironmentVariable("BROWSER", "Machine")) {
-    Start-Process -FilePath "$env:BROWSER" -ArgumentList "http://localhost:3000"
-} else {
-    Start-Process -FilePath "explorer.exe" -ArgumentList "http://localhost:3000"
-}
+Start-Process -FilePath "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -ArgumentList "http://localhost:3000" -NoNewWindow
